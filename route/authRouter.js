a4d45e37f4608cb679c0c4f5eebc800fcdcd5605
Novel272 <mohammedalmustafak@gmail.com>
2026-05-04 15:29:26 +0400

    Remove node_modules from repository

--- route/authRouter.js
+++
@@ -1,9 +0,0 @@
-import { AuthController, loginUser } from "../controller/authController";
-import express from "express";
-
-const AuthRouter = express.Router();
-
-AuthRouter.post("/register", AuthController);
-AuthRouter.post("/login", loginUser);
-
-export default AuthRouter;
